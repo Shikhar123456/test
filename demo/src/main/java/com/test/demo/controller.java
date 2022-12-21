91dcd84203fdaf00ac9daa0da9ffefde96bd3d22
Shikhar Bijpuria <71264645+Shikhar123456@users.noreply.github.com>
2022-12-21 11:03:39 +0530

    Update controller.java

--- demo/src/main/java/com/test/demo/controller.java
+++ demo/src/main/java/com/test/demo/controller.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class controller {
-    @GetMapping("/")
+    @GetMapping("/get")
     public String get() {
         return "Hello Shikhar";
     }
